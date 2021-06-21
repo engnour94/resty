deb0618ca83d33a73c882cabe143cafc3b94ee23
engnour94 <nour.abuelenein@hotmail.com>
2021-06-22 00:04:38 +0300

    done

--- resty/src/result.jsx
+++ resty/src/result.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./result.scss"
 import JSONPretty from 'react-json-pretty';
 class Result extends React.Component {
    render(){
@@ -6,9 +7,9 @@ class Result extends React.Component {
     return(
         
         <div className="App-result"> 
-          <h3>Header: <JSONPretty  data={this.props.Headers} ></JSONPretty>  </h3>
-          <h3> Count:{this.props.count}</h3>
-          <h3>Result:<JSONPretty  data={this.props.results} ></JSONPretty>  </h3>
+          <h3>Header:  <JSONPretty  data={this.props.Headers} ></JSONPretty>  </h3>
+          <h3>Count:  {this.props.count}</h3>
+          <h3>Result:  <JSONPretty  data={this.props.results} ></JSONPretty>  </h3>
            
         </div>
         
